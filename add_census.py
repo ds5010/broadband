7a9f6d64557a2034bc6a36c95a5a7d2bbee8ea6f
Philip <philip.eng.mathieu@gmail.com>
2022-04-14 02:38:17 -0400

    Switch to requests and upload output

--- add_census.py
+++ add_census.py
@@ -2,13 +2,11 @@
 query = "https://api.census.gov/data/2020/dec/pl?get=NAME,COUNTY,P1_001N&for=county:*&in=state:23"
 
 # Query that URL
-import urllib3
-http = urllib3.PoolManager()
-r = http.request('GET', query)
+import requests
+r = requests.get(query)
 
 # Decode the data and unload it as json
-import json
-data = json.loads(r.data.decode('utf-8'))
+data = r.json()
 
 # Transfer json into a pandas dataframe
 import pandas as pd
@@ -17,6 +15,6 @@ df["COUNTY"] = df["NAME"].str.split(" County", expand=True)[0]
 
 # Load the geopandas counties and merge the pandas dataframe in
 import geopandas as gpd
-counties = gpd.read_file("geojson/Maine_County_Boundary_Polygons_Dissolved_Feature.geojson")
+counties = gpd.read_file("Maine_County_Boundary_Polygons_Dissolved_Feature.geojson")
 counties_df = counties.merge(df, on="COUNTY") # Note: You should always run .merge() on the geodataframe not the pandas dataframe
-counties_df.to_file("geojson/Maine_County_Boundary_Polygons_Dissolved_Feature_Census_Merge.geojson", driver='GeoJSON')
+counties_df.to_file("Maine_County_Boundary_Polygons_Dissolved_Feature_Census_Merge.geojson", driver='GeoJSON')
