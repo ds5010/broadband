218eff36047ff0fa4c9eda7fa8930be32bea34f2
CCLynch <98131348+CCLynch@users.noreply.github.com>
2022-04-28 17:52:54 -0400

    fix paths in split file

--- src/split_by_maine_county.py
+++ src/split_by_maine_county.py
@@ -1,8 +1,12 @@
+'''
+Python script to take statewide layers of broadband data in geoJSON, 
+including polygons, lines, and points, then split into county-level directories   
+'''
+
 import geopandas as gpd
 from pathlib import Path
 
-from pytz import country_names
-
+# returns list of files within directory
 def list_files(dir):
     files = []
     for item in dir.iterdir():
@@ -29,21 +33,25 @@ def store_county_data_to_file(county_dict, county_name,layers,layer_name):
     del gdf['centroid_column']
     gdf.set_geometry('geometry')
 
-    gdf.to_file('county/'+county_name+'/'+county_name+'_'+layer_name+'.geojson')
+    gdf.to_file('../county/'+county_name+'/'+county_name+'_'+layer_name+'.geojson')
 
 def main():
-    zip_dir = Path('zip/')
-    county_path= Path('zip/county_boundaries/Maine_County_Boundary_Polygons_Dissolved_Feature.geojson')
-    county_polygons=gpd.read_file(county_path)
-    
+    # zip director is location of all raw data
+    zip_dir = Path('../zip/')
+    # county path is used to split up raw data into county-based subdirectories
+    county_path= Path('../zip/county_boundaries/maine_counties.geojson')
+
+    # Dissolve groups all the polygons in the same county and creates a multipolygon for each county
+    # COUNTY column becomes the index
+    county_polygons=gpd.read_file(county_path).dissolve(by='COUNTY', aggfunc='sum')
     county_dict={}
-    for i,row in county_polygons.iterrows():
-        county_dict[row['COUNTY']]=row.geometry
+    for county,row in county_polygons.iterrows():
+        county_dict[county]=row.geometry
     
     layers_dict = get_files(list_files(zip_dir))
     for file in layers_dict:
         for i in county_dict:
-            Path("county/"+i+'/').mkdir(parents=True, exist_ok=True)
+            Path("../county/"+i+'/').mkdir(parents=True, exist_ok=True)
             store_county_data_to_file(county_dict,i,layers_dict,file)
 
 if __name__ == '__main__':
